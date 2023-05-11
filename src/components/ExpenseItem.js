aa0de6bce7b3cc38f33dea4e8b24e595679aec74
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-05-11 09:54:17 +0200

    Dati passati tramite props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 //Importo la personalizzazione
 import "./ExpenseItem.css";
 // Componente
-function ExpanseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+//Aggiungiamo il parametro props(oggetto) per aggiungere i parametri delle props
+function ExpanseItem(props) {
+  // const expenseDate = new Date(2021, 2, 28);
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 294.67;
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
