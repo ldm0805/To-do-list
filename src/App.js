04d7cdbccc374d7f341456e2137ccabe070181d9
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-05-11 09:50:39 +0200

    Primo esempio props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 // App principale
 // Importo il componente
 import ExpanseItem from "./components/ExpenseItem";
+
+const expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "Car Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e3",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e4",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+];
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpanseItem></ExpanseItem>
+      <ExpanseItem
+        //props
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpanseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpanseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpanseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
