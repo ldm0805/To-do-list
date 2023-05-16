efb80251af9490f2cdc14a7960c61feccaa8c791
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-05-16 10:21:19 +0200

    Catturare i dati singolarmente con più state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,50 @@
+import { useState } from "react";
 import "./ExpenseForm.scss";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  //Titolo
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.title);
+  };
+
+  //Amount
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.amount);
+  };
+
+  //Data
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.date);
+  };
+
   return (
     <form action="">
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023/05/16" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2023/05/16"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
