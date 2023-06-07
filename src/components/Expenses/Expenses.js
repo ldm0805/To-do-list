ff53bfc63cf6facc92cc149ec9a49a5bb4171676
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-06-07 10:23:44 +0200

    miglioramento visualizzazione messaggio no expanse

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,13 +8,26 @@ const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    // console.log("expense.js");
-    // console.log(selectedYear);
   };
 
+  //Filtro expense per anno
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+
+  let expensesContent = <p>No expenses found.</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        // Aggiungo la key per aiutare la renderizzazione di react
+        // Array di expense dinamico con metodo map (Da array di oggetti ad array di jsx element)
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
 
   return (
     <Card className="expenses">
@@ -24,21 +37,7 @@ const Expenses = (props) => {
       />
 
       {/* Condizioni se in quell'anno non ci sono expense*/}
-
-      {filteredExpenses.length === 0 ? (
-        <p>No expenses found</p>
-      ) : (
-        filteredExpenses.map((expense) => (
-          <ExpenseItem
-            // Aggiungo la key per aiutare la renderizzazione di react
-            // Array di expense dinamico con metodo map (Da array di oggetti ad array di jsx element)
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))
-      )}
+      {expensesContent}
     </Card>
   );
 };
